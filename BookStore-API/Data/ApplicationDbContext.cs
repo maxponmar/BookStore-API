03d0317a44b0e0ba3b7676b87a23e42585fbf2c7
Maximiliano <maxponce.marquez@gmail.com>
2020-08-13 00:27:16 -0700

    Models and DTO's created, and automapper included

--- BookStore-API/Data/ApplicationDbContext.cs
+++ BookStore-API/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BookStore_API.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ namespace BookStore_API.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
