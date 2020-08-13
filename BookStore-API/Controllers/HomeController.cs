b7e9ad0d5e398d178a07ccb46ba7e5fff37a91ca
Maximiliano <maxponce.marquez@gmail.com>
2020-08-12 18:39:42 -0700

    Add project files.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a test API controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _loggerService;
+        public HomeController(ILoggerService loggerService)
+        {
+            _loggerService = loggerService;
+        }
+
+        /// <summary>
+        /// Gets two strings
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _loggerService.LogInfo("Access to Home Controller - TESTING");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _loggerService.LogDebug("Debugging message - TESTING");
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _loggerService.LogError("Error message - TESTING");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _loggerService.LogWarn("Delete, Home Controller - TESTING");
+        }
+    }
+}
