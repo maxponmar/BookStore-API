db7c37f089c87109f69e1761627253d4521d2305
Maximiliano <maxponce.marquez@gmail.com>
2020-08-14 15:00:10 -0700

    Updating endpoints

--- BookStore-UI/Static/Endpoints.cs
+++ BookStore-UI/Static/Endpoints.cs
@@ -7,7 +7,7 @@ namespace BookStore_UI.Static
 {
     public static class Endpoints
     {
-        public static string BaseUrl = "http://localhost:50292/";
+        public static string BaseUrl = "https://localhost:44381/";
         public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
         public static string BooksEndpoint = $"{BaseUrl}api/books/";
         public static string RegisterEndpoint = $"{BaseUrl}api/users/register/";
